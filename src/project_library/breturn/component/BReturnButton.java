bf8b5bab65d2de7b95cd547cc6575b3cc2ba766a
seungeun <tmddms0931@naver.com>
2020-08-20 21:00:36 +0900

    BRturn 패키지 생성 및 스윙UI작업

---
+++ src/project_library/breturn/component/BReturnButton.java
@@ -0,0 +1,34 @@
+package project_library.breturn.component;
+
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.border.EmptyBorder;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class BReturnButton extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public BReturnButton() {
+		setBorder(new EmptyBorder(0, 10, 0, 10));
+		
+		JButton btnReturn = new JButton("반납");
+		btnReturn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		add(btnReturn);
+		
+		JButton btnExit = new JButton("닫기");
+		btnExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		add(btnExit);
+
+	}
+
+}
---
+++ src/project_library/breturn/component/BReturnTable.java
@@ -0,0 +1,11 @@
+package project_library.breturn.component;
+
+import javax.swing.JTable;
+import javax.swing.border.EmptyBorder;
+
+public class BReturnTable extends JTable {
+	public BReturnTable() {
+		setBorder(new EmptyBorder(0, 10, 0, 10));
+	}
+
+}
