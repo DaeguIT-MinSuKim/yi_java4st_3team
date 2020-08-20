98d9232e3d8421e859101f14289f98ca53fa0b38
digi5633 <digi5633@gmail.com>
2020-08-20 21:46:51 +0900

    BOOK 목록 추가

--- Scripts/user_lib-data.sql
+++ Scripts/user_lib-data.sql
@@ -8,3 +8,14 @@ INSERT INTO MEMBER VALUES('M006', '박바자', '010-6785-6785', 0);
 INSERT INTO MEMBER VALUES('M007', '김자자', '010-7856-7856', 0);
 INSERT INTO MEMBER VALUES('M008', '이자자', '010-8567-8567', 0);
 INSERT INTO MEMBER VALUES('M009', '박자자', '010-0000-0000', 0);
+
+-- BOOK 목록
+INSERT INTO BOOK VALUES('B001', '리눅스 시스템 구축', '신윤환', '북수홀릭', 33000, 1, 0);
+INSERT INTO BOOK VALUES('B002', 'HTML5 + CSS3', '고경희', '이지스퍼블리싱', 25000, 1, 0);
+INSERT INTO BOOK VALUES('B003', '자바스크립트 기본 편', '고경희', '이지스퍼블리싱', 25000, 1, 0);
+INSERT INTO BOOK VALUES('B004', '자바스크립트 + 제이쿼리 입문', '정인용', '이지스퍼블리싱', 20000, 1, 0);
+INSERT INTO BOOK VALUES('B005', 'Java의 정석', '남궁성', '도우출판', 25000, 1, 0);
+INSERT INTO BOOK VALUES('B006', '오라클로 배우는 데이터베이스', '이지훈', '이지스퍼블리싱', 25000, 1, 0);
+INSERT INTO BOOK VALUES('B007', '자바 프로젝트 필수 유틸리티', '전민수', '한빛미디어', 35000, 1, 0);
+INSERT INTO BOOK VALUES('B008', '자바 프로그래밍 입문', '박은종', '이지스퍼블리싱', 25000, 1, 0);
+INSERT INTO BOOK VALUES('B009', 'C 언어 입문', '김성엽', '이지스퍼블리싱', 25000, 1, 0);
