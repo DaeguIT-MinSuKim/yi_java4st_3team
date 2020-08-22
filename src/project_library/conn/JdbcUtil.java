3a1f3795cacdca7979451ba4ea492c42eef5e344
daehyun <daehyun@DESKTOP-CH7NJ48>
2020-08-22 16:18:43 +0900

    JdbcUtill 계정연결 오류로 인한 임시해결

--- src/project_library/conn/JdbcUtil.java
+++ src/project_library/conn/JdbcUtil.java
@@ -11,23 +11,29 @@ public class JdbcUtil {
 
 	public static Connection getConnection() {
 		Connection conn = null;
-		String proptiesPath = "db.properties";
+		//String proptiesPath = "db.properties";
 		// String proptiesPath = "mysql_db.properties";
-		try (InputStream is = ClassLoader.getSystemResourceAsStream(proptiesPath)) {
-			Properties props = new Properties();
-			props.load(is);
+		try {//(InputStream is = ClassLoader.getSystemResourceAsStream(proptiesPath)) {
+			//Properties props = new Properties();
+			//props.load(is);
 			// System.out.println(props);
-			String url = props.getProperty("url");
-			// String user = props.getProperty("user");
-			// String password = props.getProperty("password");
+			//String url = props.getProperty("url");
+			 //String user = props.getProperty("user");
+			 //String password = props.getProperty("password");
+			 String url = "jdbc:oracle:thin:@localhost:1521:orcl?useSSL=false";
+			 String user = "user_lib";
+			 String password = "rootroot";
 			// System.out.printf("user = %s, password = %s, url = %s%n", url, user,
 			// password);
-			// conn = DriverManager.getConnection(url, user, password);
-			conn = DriverManager.getConnection(url, props);
-		} catch (IOException e) {
+			conn = DriverManager.getConnection(url, user, password);
+			//conn = DriverManager.getConnection(url, props);
+		}
+		//catch (IOException e) {
+		//	System.out.println(e.getMessage());
+		//	e.printStackTrace();
+		//} 
+		catch (Exception e) {
 			System.out.println(e.getMessage());
-			e.printStackTrace();
-		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return conn;
