c3729f1f97be3d121afc50616c872e5b8bf7aa75
DAEHYUN KIM <32122611+webcogy@users.noreply.github.com>
2020-08-24 16:05:59 +0900

    JdbcUtill 수정

--- src/project_library/conn/JdbcUtil.java
+++ src/project_library/conn/JdbcUtil.java
@@ -1,11 +1,7 @@
 package project_library.conn;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Properties;
 
 public class JdbcUtil {
 
