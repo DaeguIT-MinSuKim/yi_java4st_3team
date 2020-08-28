ad90d7b14ce92aa0ba83d4ebfd9977964b3f5999
digi5633 <digi5633@gmail.com>
2020-08-28 17:00:29 +0900

    도서현황 완료

--- src/project_library/ui/component/BookStatusPanel.java
+++ src/project_library/ui/component/BookStatusPanel.java
@@ -16,6 +16,8 @@ public class BookStatusPanel extends JPanel {
 	private BookStatusManagementService bService;
 
 	public BookStatusPanel() {
+		bService = new BookStatusManagementService();
+		
 		setBorder(new TitledBorder(null, "[ 도서 현황 ]", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		setLayout(new GridLayout(0, 2, 20, 10));
 		
@@ -25,23 +27,25 @@ public class BookStatusPanel extends JPanel {
 		add(lblTBook);
 		
 		JTextField tfToBook = new JTextField();
+		tfToBook.setHorizontalAlignment(SwingConstants.CENTER);
 		tfToBook.setFont(new Font("맑은 고딕", Font.BOLD, 14));
 		add(tfToBook);
 		tfToBook.setColumns(10);
 		tfToBook.setText(bService.selectTotalBook() + "권");
+		tfToBook.setEditable(false);
 
 		JLabel lblOBook = new JLabel("총 연체 권수");
 		lblOBook.setHorizontalAlignment(SwingConstants.CENTER);
 		lblOBook.setFont(new Font("맑은 고딕", Font.BOLD, 14));
 		add(lblOBook);
-		
-		bService = new BookStatusManagementService();
-		
+
 		JTextField tfOvBook = new JTextField();
+		tfOvBook.setHorizontalAlignment(SwingConstants.CENTER);
 		tfOvBook.setFont(new Font("맑은 고딕", Font.BOLD, 14));
 		add(tfOvBook);
 		tfOvBook.setColumns(10);
-		//bService.selectTotalBook();
+		tfOvBook.setText(bService.selectTotalOverdueBook() + "권");
+		tfOvBook.setEditable(false);
 		
 		JLabel lblRBook = new JLabel("총 대여 권수");
 		lblRBook.setHorizontalAlignment(SwingConstants.CENTER);
@@ -49,9 +53,12 @@ public class BookStatusPanel extends JPanel {
 		add(lblRBook);
 		
 		JTextField tfReBook = new JTextField();
+		tfReBook.setHorizontalAlignment(SwingConstants.CENTER);
 		tfReBook.setFont(new Font("맑은 고딕", Font.BOLD, 14));
 		add(tfReBook);
 		tfReBook.setColumns(10);
+		tfReBook.setText(bService.selectTotalRentalBook() + "권");
+		tfReBook.setEditable(false);
 	}
 	
 	/*	// 수정 불가능
